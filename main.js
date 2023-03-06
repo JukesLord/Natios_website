d86a829dea568643e4721ebe183643ef41c95eb0
JukesLord <flphosek@seznam.cz>
2023-03-06 13:50:49 +0100

    dsa
    dsa

--- main.js
+++ main.js
@@ -31,3 +31,38 @@ function showSummary() {
   summaryPage.classList.remove("inactive");
   summaryPage.classList.add("active");
 }
+
+
+let box = document.querySelector('.web-content');
+let pos = box.offsetTop;
+
+$(window).bind('mousewheel', function(event) {
+  setTimeout(() => {
+    
+  
+  if (event.originalEvent.wheelDelta >= 0) {
+      console.log('Scroll up');
+          window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+          });
+          window.onscroll=function(){};
+  }
+  else {
+      console.log('Scroll down');
+      window.scrollTo({
+          top: pos,
+          behavior: 'smooth'
+          });
+          window.onscroll=function(){};
+  }
+}, 100);
+  });
+
+var x=window.scrollX;
+var y=window.scrollY;
+window.onscroll=function(){window.scrollTo(x, y);};
+
+setTimeout(() => {
+  console.log("this is the first message");
+}, 5000);
